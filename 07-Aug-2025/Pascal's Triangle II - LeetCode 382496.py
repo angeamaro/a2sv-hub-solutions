ada15c014045cadb95fbf5e8a205103c4370454f
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-07 10:41:12 +0100

    Created 'Pascal's Triangle II - LeetCode' by A2SV Hub

---
+++ 07-Aug-2025/Pascal's Triangle II - LeetCode 382496.py
@@ -0,0 +1,22 @@
+# Problem: Pascal's Triangle II - LeetCode - https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+
+        triangle = [[1]]
+        for i in range(1,rowIndex + 1):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            triangle.append(row)
+        return triangle[rowIndex]
+        
+
+
+        
+        
