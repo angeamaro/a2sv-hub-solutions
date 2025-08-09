18a221965706d24e3761e5de26d43e788378d477
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-09 14:16:33 +0100

    Created 'Partition Equal Subset Sum' by A2SV Hub

---
+++ 09-Aug-2025/Partition Equal Subset Sum 383116.py
@@ -0,0 +1,15 @@
+# Problem: Partition Equal Subset Sum - https://leetcode.com/problems/partition-equal-subset-sum/
+
+ total = sum(nums)
+        if total % 2 != 0:
+            return False
+        
+        target = total // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+                
+        return dp[target]
