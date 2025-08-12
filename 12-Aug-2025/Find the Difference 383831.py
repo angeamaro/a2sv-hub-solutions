296eae5ccdcf7a02db3d1e0c794e9f937aaed243
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-12 13:03:44 +0100

    Created 'Find the Difference' by A2SV Hub

---
+++ 12-Aug-2025/Find the Difference 383831.py
@@ -0,0 +1,9 @@
+# Problem: Find the Difference - https://leetcode.com/problems/find-the-difference/
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count_s = Counter(s)
+        count_t = Counter(t)
+        for ch in count_t:
+            if count_t[ch] > count_s.get(ch, 0):
+                return ch
