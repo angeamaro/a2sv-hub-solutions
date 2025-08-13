4189072ad0dfb644a46a4139705e1de1b0c1d489
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-13 21:01:33 +0100

    Created 'E - Zeroing Array' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 13-Aug-2025/E - Zeroing Array 384789.py
@@ -0,0 +1,16 @@
+# Problem: E - Zeroing Array - https://codeforces.com/gym/628023/problem/E
+
+n = int(input())
+
+arr = list(map(int,input().split()))
+
+total = maximum = 0
+
+for num in arr:
+    total += num
+    maximum = max(maximum, num)
+
+if total % 2 == 1 or maximum > total - maximum:
+    print('NO')
+else:
+    print('YES')
