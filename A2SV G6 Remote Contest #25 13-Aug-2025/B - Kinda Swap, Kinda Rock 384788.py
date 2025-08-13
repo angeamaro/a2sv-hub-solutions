274dcb8a52deaaf80162ec8e43e679a69977b481
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-13 21:01:23 +0100

    Created 'B - Kinda Swap, Kinda Rock' by A2SV Hub

---
+++ A2SV G6 Remote Contest #25 13-Aug-2025/B - Kinda Swap, Kinda Rock 384788.py
@@ -0,0 +1,18 @@
+# Problem: B - Kinda Swap, Kinda Rock - https://codeforces.com/gym/628023/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    a.sort()
+    b.sort(reverse=True)
+
+    for i in range(min(k, n)):
+        if b[i] > a[i]:
+            a[i], b[i] = b[i], a[i]
+        else:
+            break
+
+    print(sum(a))
