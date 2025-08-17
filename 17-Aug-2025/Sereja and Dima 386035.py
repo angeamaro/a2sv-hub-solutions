46e82ff0eb89a195b74e8879c95cd01d0fa8c895
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-17 19:34:43 +0100

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 17-Aug-2025/Sereja and Dima 386035.py
@@ -0,0 +1,25 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+l, r = 0, n - 1
+sereja = dima = 0
+turn = 0  
+
+while l <= r:
+    if arr[l] > arr[r]:
+        x = arr[l]
+        l += 1
+    else:
+        x = arr[r]
+        r -= 1
+
+    if turn == 0:
+        sereja += x
+    else:
+        dima += x
+
+    turn ^= 1  
+
+print(sereja, dima)
