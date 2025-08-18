395a27bcb7ecd884753d02087f0ac43f42b67cbe
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-18 17:20:06 +0100

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 18-Aug-2025/D - Mugisho and Rufeyda 386529.py
@@ -0,0 +1,10 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+n, t = map(int, input().split())
+
+if n == 1 and t == 10:
+    print(-1)
+elif t == 10:
+    print("1" + "0" * (n - 1))
+else:
+    print(str(t) + "0" * (n - 1))
