5baca00aac6a3fbd8680c8be740c09458d7d5bf1
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-08-24 21:20:57 +0100

    Created 'A - Xorion and the Peaks' by A2SV Hub

---
+++ A2SV Contest #27 24-Aug-2025/A - Xorion and the Peaks 388583.py
@@ -0,0 +1,14 @@
+# Problem: A - Xorion and the Peaks - https://codeforces.com/gym/630556/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    if k > (n - 1) // 2:
+        print(-1)
+        continue
+    
+    a = list(range(1, n+1))    
+    for i in range(1, k+1):
+        a[2*i-1], a[2*i] = a[2*i], a[2*i-1]
+        
+    print(" ".join(map(str, a)))
