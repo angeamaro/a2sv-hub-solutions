f06d0b4dac7d9f452a84e157e099759c17fb5555
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-09-05 18:14:05 +0100

    Created 'Count Servers that Communicate' by A2SV Hub

---
+++ 05-Sep-2025/Count Servers that Communicate 391433.py
@@ -0,0 +1,23 @@
+# Problem: Count Servers that Communicate - https://leetcode.com/problems/count-servers-that-communicate/description/
+
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        
+        row = [0] * m
+        col = [0] * n
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    row[i] += 1
+                    col[j] += 1
+        
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (row[i] > 1 or col[j] > 1):
+                    res += 1
+        
+        return res
+        
