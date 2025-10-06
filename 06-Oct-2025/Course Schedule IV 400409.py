a546159919d41daa8ea55eaab73f0898eaea3343
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-10-06 21:27:10 +0100

    Created 'Course Schedule IV' by A2SV Hub

---
+++ 06-Oct-2025/Course Schedule IV 400409.py
@@ -0,0 +1,17 @@
+# Problem: Course Schedule IV - https://leetcode.com/problems/course-schedule-iv/description/
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        reachable = [[False] * numCourses for _ in range(numCourses)]
+        for a, b in prerequisites:
+            reachable[a][b] = True
+
+        for k in range(numCourses):
+            for i in range(numCourses):
+                if reachable[i][k]:
+                    for j in range(numCourses):
+                        if reachable[k][j]:
+                            reachable[i][j] = True
+
+        return [reachable[u][v] for u, v in queries]
+        
