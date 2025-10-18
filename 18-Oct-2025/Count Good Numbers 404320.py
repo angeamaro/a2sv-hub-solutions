02ebefc84f99d1f158a8307d64aee2b41c382bf9
Ângela Amaro <angela.amaro.ar6@gmail.com>
2025-10-18 22:36:19 +0100

    Created 'Count Good Numbers' by A2SV Hub

---
+++ 18-Oct-2025/Count Good Numbers 404320.py
@@ -0,0 +1,11 @@
+# Problem: Count Good Numbers - https://leetcode.com/problems/count-good-numbers/
+
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10**9 + 7
+        
+        even = (n + 1) // 2
+        odd = n // 2
+        
+        return (pow(5, even, MOD) * pow(4, odd, MOD)) % MOD
+        
